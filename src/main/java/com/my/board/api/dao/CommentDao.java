f836ccac73de893410093d602df8f77018ddb16f
zzzmini <sm01079200887@gmail.com>
2025-09-01 13:14:10 +0900

    댓글 추가 API 처리완료

--- src/main/java/com/my/board/api/dao/CommentDao.java
+++ src/main/java/com/my/board/api/dao/CommentDao.java
@@ -1,5 +1,6 @@
 package com.my.board.api.dao;
 
+import com.my.board.entity.Article;
 import com.my.board.entity.Comment;
 import jakarta.persistence.EntityManager;
 import jakarta.transaction.Transactional;
@@ -15,5 +16,15 @@ public class CommentDao {
     // 1. Comment 찾기
     public Comment findComment(Long commentId) {
         return em.find(Comment.class, commentId);
+    }
+
+    public void insertComment(Long articleId, Comment comment) {
+        // 1. 해당 게시글을 찾는다.
+        Article article = em.find(Article.class, articleId);
+        // 2. comment 엔티티에 article을 할당
+        comment.setArticle(article);
+        // 3. comment를 게시글에 리스트로 추가한다.
+        article.getComments().add(comment);
+        em.persist(article);
     }
 }
